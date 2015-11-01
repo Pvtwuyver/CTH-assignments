c8125b2afbf945a1cc21b214894eec0b6586c8a8
pvtwuyver <pvtwuyver@gmail.com>
2015-11-01 11:01:46 +0000

    Add three scripts to the repository

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+var turnImage_right = function() {
+    $('#image')[0].style.transform = "rotate(90deg)";
+}
+
+var turnImage_left = function() {
+    $('#image')[0].style.transform = "rotate(-90deg)";
+}
+var turnImage_180 = function() {
+    $('#image')[0].style.transform = "rotate(180deg)";
+}
+var turnImage_original = function() {
+    $('#image')[0].style.transform = "rotate(0deg)";
+}
